59e2f4d369ab7aafeb89602f8eafa2acad164c66
Hamza Nasry <family@Computer-1.local>
2023-05-24 17:00:55 -0700

    Files instead of url for images

--- script.js
+++ script.js
@@ -3,20 +3,17 @@ document.addEventListener("DOMContentLoaded", function() {
     var iconButton = document.getElementById("icon");
     var googleSearchButton = document.getElementById("googlesearch");
   
-    // Add event listener for Enter key press
     searchInput.addEventListener("keydown", function(event) {
-      if (event.key === "Enter") { // Check if Enter key is pressed
+      if (event.key === "Enter") {
         googleSearch();
-        event.preventDefault(); // Prevent form submission or page reload
+        event.preventDefault(); 
       }
     });
   
-    // Add event listener for icon button click
     iconButton.addEventListener("click", function() {
       googleSearch();
     });
   
-    // Add event listener for googlesearch button click
     googleSearchButton.addEventListener("click", function(event) {
       googleSearch();
       event.preventDefault();
